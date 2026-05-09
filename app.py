12ccafcc5c0bc9beb64f141fbdd5ec9bdd3cc039
Codnell <151796990+Napster-12@users.noreply.github.com>
2026-05-09 03:15:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -152,21 +152,22 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # ==============================
-# MAIL CONFIG
+# MAIL CONFIGURATION (RENDER READY)
 # ==============================
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
 
-app.config['MAIL_USERNAME'] = 'codnellsmall@gmail.com'
+app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS', 'True') == 'True'
+app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL', 'False') == 'True'
 
-# IMPORTANT:
-# Replace with your REAL Gmail App Password
-app.config['MAIL_PASSWORD'] = 'rebe zhfq blgw yzih'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
-app.config['MAIL_DEFAULT_SENDER'] = 'codnellsmall@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get(
+    'MAIL_DEFAULT_SENDER',
+    app.config['MAIL_USERNAME']
+)
 
 mail = Mail(app)
 
